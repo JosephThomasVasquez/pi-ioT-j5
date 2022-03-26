44c78ad024f0ca1dc6f42bf146fd85464d805164
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-03-26 13:43:08 -0700

    Added timer using setInterval

--- app_led.js
+++ app_led.js
@@ -4,22 +4,44 @@ const raspio = require("raspi-io").RaspiIO;
 const board = new five.Board({ io: new raspio() });
 
 board.on("ready", function () {
-  let count = 10;
+  let count = 1;
 
   const redLed = new five.Led("P1-12");
   const yellowLed = new five.Led("P1-7");
   const greenLed = new five.Led("P1-11");
   //   five; //   greenLed.off(); //   yellowLed.off(); //   redLed.off();
 
-  redLed.on();
+  //   redLed.on();
 
-  for (let i = 1; i < count; i++) {
-    redLed.on();
-    console.log("tick", count);
-    setTimeout(() => {
+  //   for (let i = 1; i < count; i++) {
+  //     redLed.on();
+  //     console.log("tick", count);
+  //     setTimeout(() => {
+  //       redLed.off();
+  //     }, 1000);
+  //   }
+
+  setInterval(() => {
+    if (count <= 10) {
+      count++;
+      redLed.on();
+      yellowLed.off();
+      greenLed.off();
+      console.log("tick", count);
+    }
+
+    if (count === 11) {
+      count = 1;
       redLed.off();
-    }, 1000);
-  }
+      yellowLed.off();
+      greenLed.on();
+      console.log("tick", count);
+    }
+
+    // setTimeout(() => {
+    //   redLed.off();
+    // }, 1000);
+  }, 1000);
 
   //   if (redLed.on()) {
   //     console.log("Red");
