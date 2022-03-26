10cb7122b1ed0cf8a6d4003c13a6e2da6a77f880
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-03-25 21:24:33 -0700

    Added yellow and green leds and logic to switch between traffic lights

--- app_led.js
+++ app_led.js
@@ -4,7 +4,29 @@ const raspio = require("raspi-io").RaspiIO;
 const board = new five.Board({ io: new raspio() });
 
 board.on("ready", function () {
-  const led = new five.Led("P1-12");
+  const count = 1;
 
-  led.on();
+  const redLed = new five.Led("P1-12");
+  const yellowLed = new five.Led("P1-7");
+  const greenLed = new five.Led("P1-11");
+
+  if (count === 1) {
+    redLed.on();
+    setTimeout(function () {
+      redLed.off();
+      count++;
+    }, 10000);
+
+    yellowLed.on();
+    setTimeout(function () {
+      yellowLed.off();
+      count++;
+    }, 2000);
+
+    greenLed.on();
+    setTimeout(function () {
+      greenLed.off();
+      count = 1;
+    }, 10000);
+  }
 });
