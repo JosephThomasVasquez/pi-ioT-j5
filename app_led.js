d7f55edba8f7b8199ce626b54804247ffa4fe285
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-03-25 21:10:03 -0700

    Turn on led

--- app_led.js
+++ app_led.js
@@ -4,5 +4,7 @@ const raspio = require("raspi-io").Raspio();
 const board = new five.Board({ io: new raspio() });
 
 board.on("ready", function () {
-  const led = five.Led(12);
+  const led = five.Led("PI-12");
+
+  led.on();
 });
