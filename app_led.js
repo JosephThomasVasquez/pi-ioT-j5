c1afab58dbbf1de572f66254b4ac0ab12e5681aa
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-03-26 13:56:39 -0700

    Added increment on green Led

--- app_led.js
+++ app_led.js
@@ -4,7 +4,7 @@ const raspio = require("raspi-io").RaspiIO;
 const board = new five.Board({ io: new raspio() });
 
 board.on("ready", function () {
-  let count = 1;
+  let count = 0;
 
   const trafficLightSwitch = {
     red: 10,
@@ -15,17 +15,6 @@ board.on("ready", function () {
   const redLed = new five.Led("P1-12");
   const yellowLed = new five.Led("P1-7");
   const greenLed = new five.Led("P1-11");
-  //   five; //   greenLed.off(); //   yellowLed.off(); //   redLed.off();
-
-  //   redLed.on();
-
-  //   for (let i = 1; i < count; i++) {
-  //     redLed.on();
-  //     console.log("tick", count);
-  //     setTimeout(() => {
-  //       redLed.off();
-  //     }, 1000);
-  //   }
 
   setInterval(() => {
     // Set Red to On
@@ -51,6 +40,7 @@ board.on("ready", function () {
       count <= trafficLightSwitch.green &&
       count > trafficLightSwitch.yellow
     ) {
+      count++;
       redLed.off();
       yellowLed.off();
       greenLed.on();
@@ -59,31 +49,9 @@ board.on("ready", function () {
 
     // Set count back to 1
     if (count > trafficLightSwitch.green) {
-      count = 1;
+      count = 0;
     }
   }, 1000);
-
-  //   if (redLed.on()) {
-  //     console.log("Red");
-
-  //     setTimeout(function () {
-  //       redLed.off();
-
-  //       console.log("Yellow");
-  //       yellowLed.on();
-  //       setTimeout(function () {
-  //         yellowLed.off();
-
-  //         console.log("Green");
-  //         greenLed.on();
-  //         setTimeout(function () {
-  //           greenLed.off();
-
-  //           redLed.on();
-  //         }, 10000);
-  //       }, 2000);
-  //     }, 10000);
-  //   }
 });
 
 const switchLights = () => {};
